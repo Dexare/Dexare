920890bc07fa1aa4feaa2ccdbd5ff98ddac8effd
Snazzah <suggesttosnazzy@gmail.com>
2021-02-14 05:20:58 -0600

    chore: Make `util.iterateFolder` have an extension argument

--- lib/util/index.d.ts
+++ lib/util/index.d.ts
@@ -2,7 +2,8 @@
  * Iterates through a folder and calls back on every .js found.
  * @param folder The path to check
  * @param callback The function to call on each file found
+ * @param extension The extension to look for
  */
-export declare function iterateFolder(folder: string, callback: (path: string) => Promise<any>): Promise<any>;
+export declare function iterateFolder(folder: string, callback: (path: string) => Promise<any>, extension?: string): Promise<any>;
 export declare function escapeRegex(str: string): string;
 export declare function truncate(text: string, limit?: number): string;
--- src/util/index.ts
+++ src/util/index.ts
@@ -5,10 +5,12 @@ import * as fs from 'fs';
  * Iterates through a folder and calls back on every .js found.
  * @param folder The path to check
  * @param callback The function to call on each file found
+ * @param extension The extension to look for
  */
 export async function iterateFolder(
   folder: string,
-  callback: (path: string) => Promise<any>
+  callback: (path: string) => Promise<any>,
+  extension = '.js'
 ): Promise<any> {
   const files = fs.readdirSync(folder);
   return Promise.all(
@@ -17,12 +19,12 @@ export async function iterateFolder(
       const stat = fs.lstatSync(filePath);
       if (stat.isSymbolicLink()) {
         const realPath = fs.readlinkSync(filePath);
-        if (stat.isFile() && file.endsWith('.js')) {
+        if (stat.isFile() && file.endsWith(extension)) {
           return callback(realPath);
         } else if (stat.isDirectory()) {
           return iterateFolder(realPath, callback);
         }
-      } else if (stat.isFile() && file.endsWith('.js'))
+      } else if (stat.isFile() && file.endsWith(extension))
         return callback(filePath);
       else if (stat.isDirectory()) return iterateFolder(filePath, callback);
     })
